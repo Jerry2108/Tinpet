034887b576994c8118ff0fc0c2ace4aa990ec64d
Jerry <ngocbaol@student.unimelb.edu.au>
2021-03-10 21:25:59 +1100

    Completed the website

---
+++ css/script.js
@@ -0,0 +1,14 @@
+/*change the color of icons*/
+$(".icon").addClass("color1");
+/*when u click into an icon, its color is gonna change*/
+
+
+/*$(".icon").click(function(){
+  $(".icon").addClass("color2");
+});
+$(".bullseye").click(function(){
+  $("this").addClass("color2");
+});
+$(".circle").click(function(){
+  $("this").addClass("color2");
+});*/
